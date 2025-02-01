5f5f0a040c488d48048a95bbe1d6074e0edf45ef
Chado <Chado9961@gmail.com>
2025-02-01 18:01:16 +0200

    add group

--- posts/admin.py
+++ posts/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
     # перечисляем поля, которые должны отображаться в админке
-    list_display = ("pk", "text", "pub_date", "author") 
+    list_display = ("pk", "text", "pub_date", "author", "group") 
     # добавляем интерфейс для поиска по тексту постов
     search_fields = ("text",) 
     # добавляем возможность фильтрации по дате
     list_filter = ("pub_date",) 
     empty_value_display = "-пусто-"
 
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "slug")
+    search_fields = ("title", "description")
+
+    
+    
+
+
+
 # при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- posts/models.py
+++ posts/models.py
@@ -3,8 +3,22 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
+class Group(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.title  # Повертає назву групи
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    group = models.ForeignKey(Group, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.text[:50]}...'  # Повертає назву групи
 
